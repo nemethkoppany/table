77d450e2c42a52a4f721d0fe82603e9f8866ebd9
nemethkoppany <koppany344@gmail.com>
2024-10-31 20:04:43 +0100

    homework is done

--- functions.js
+++ functions.js
@@ -4,13 +4,15 @@
  * @param {string} innerHTML 
  * @param {'HTMLTableRowElement} parentElement 
  */
-function createTableCell(tagName, innerHTML, parentElement){
-    const tablecell  = document.createElement(tagName)
-    tablecell.innerHTML = innerHTML;
-    parentElement.appendChild(tablecell);
-    return tablecell;
+//függvény cellakészítéshez
+function createTableCell(tagName, innerHTML, parentElement){//az elemek amiket használni fogunk
+    const tablecell  = document.createElement(tagName)//Egy cellataget csinálunk(lehet td vagy th) és azt eltároljuk egy tablecell változóban
+    tablecell.innerHTML = innerHTML; // a tablecellbe belerakja az innerHTML tartalmát, ami egy string
+    parentElement.appendChild(tablecell);//hozzáadja a tablecell-t a parentElementhez
+    return tablecell;//visszatérés tablecellel
  }
 
+ //Gyakorlatilag ugyan az mint az előző azzal a külömbséggel, hogy itt adunk az HTML_elementnek egy id-t
  function createHTMLelement(tag, id, parent){
     const HTML_element = document.createElement(tag);
     HTML_element.id = id;
@@ -18,17 +20,17 @@ function createTableCell(tagName, innerHTML, parentElement){
  }
 
  function create_HTML_element_with_parent_id(tag,id,parentid){
-    const parent_element = document.getElementById(parentid)
+    const parent_element = document.getElementById(parentid)//bekérjuk a HTML elementet id alapján és azt eltároljuk egy változóban
     
-    if(parent_element != undefined){
+    if(parent_element != undefined){ //Ha a parent_element nem üres, akkor készítsen egy HTML elementet
         createHTMLelement(tag,id,parent_element);
     }
  }
-
+//A táblázat headerjének elkészítése függvénymeghívásokkal
  function createTableHeaderCells(){
-    const tr_element = document.getElementById('person_tr');
+    const tr_element = document.getElementById('person_tr'); 
     createTableCell("th","Vezetéknév",tr_element);
-    const keresztnev = createTableCell("th","Keresztnév",tr_element);
+    const keresztnev = createTableCell("th","Keresztnév",tr_element);//a cellát eltároljuk egy változóban
     createTableCell("th","háziállat",tr_element);
     createTableCell("th","házastárs",tr_element);
     keresztnev.colSpan = 2;
@@ -36,40 +38,40 @@ function createTableCell(tagName, innerHTML, parentElement){
  
  function renderTable(person_array){
     const tbody = document.getElementById('person_tbody');
-    tbody.innerHTML = '';
-    for(const pers of person_array){
-        const tbody_tr = document.createElement('tr');
-        tbody.appendChild(tbody_tr);
+    tbody.innerHTML = '';//a tbody tartalmának törlése
+    for(const pers of person_array){//loop ami végigmegy a person_array-en egy pers változóval
+        const tbody_tr = document.createElement('tr');//tr element készítése és annak eltárolása a tbody_tr változóban
+        tbody.appendChild(tbody_tr);//És annak berakása a törzsbe
 
         createTableCell("td",  pers.lastname, tbody_tr);
       const  firstname1_cell = createTableCell("td", pers.firstname1, tbody_tr);
       
         
-        if(pers.firstname2 === undefined){
+        if(!pers.firstname2){//Ha a második keresztnév nem üres
             
-           firstname1_cell.colSpan = 2;
+           firstname1_cell.colSpan = 2;//vonja össze a az első keresztnév cellájával
             
             
         }
         else{
             
-             createTableCell("td", pers.firstname2, tbody_tr); 
+             createTableCell("td", pers.firstname2, tbody_tr); //Ha pedig üres akkor függvénymeghívással csináljon egyet
         }
 
         
         createTableCell("td", pers.pet, tbody_tr);
-        createTableCell("td", pers.married ? "Igen" : "Nem", tbody_tr );
+        createTableCell("td", pers.married ? "Igen" : "Nem", tbody_tr );//Ha a pers.married == true akkor azt írja, hogy igen, ha pedig false akkor azt írja, hogy nem (a per.married egy bool)
        
         
 
-
+          // Megkeresi az éppen kijelölt sort, ha van ilyen, és eltávolítja róla a 'selected' osztályt
         tbody_tr.addEventListener('click', function(e){
     
            const isSelected = tbody.querySelector('.selected');
             if( isSelected != undefined){
                 isSelected.classList.remove('selected');
            }
-    
+           //Hozzáadja a selected osztályt az éppen rákattintott elemhez
            e.currentTarget.classList.add('selected');
            
         })
@@ -83,29 +85,29 @@ function createTableCell(tagName, innerHTML, parentElement){
 
 
 function validateFields(lastname, firstname1, pet){
-   let result = true;
-   if(lastname.value === ""){
-       const apa = lastname.parentElement;
-       const error = apa.querySelector('.error')
-       error.innerHTML = 'Kötelező'
-       result = false;
-   }
 
-   if(firstname1.value === ""){
-       const apa = firstname1.parentElement;
-       const error = apa.querySelector('.error')
-       error.innerHTML = 'Kötelező'
-       result = false;
-   }
-
-   if(pet.value === ""){
-       const apa = pet.parentElement;
-       const error = apa.querySelector('.error')
-       error.innerHTML = 'Kötelező'
-       result = false;
-   }
-
- return result;
+let valtozo = true; //A változót beállítjuk true értékre //Ha ezek közül bármelyik üres akkor a változó értéke false lesz és kiírja, hogy "kötelező"
+ if(!validateElement(lastname)){
+   valtozo = false;
+ }
+ if(!validateElement(firstname1)){
+   valtozo = false;
+ }
+ if(!validateElement(pet)){
+   valtozo = false;
+ }
+ return valtozo;
 }
 
 
+
+function validateElement(field){
+   const parentElement = field.parentElement//a kitöltendő rublika parentelementjét eltároljuk egy változóban
+   const error = parentElement.querySelector('.error')
+   if(field.value === ""){//Ha nincs semmi beleírva a rublikába kiírja, hogy kötelező beleírni valamit és egy false értékkel tér vissza
+      error.innerHTML = 'Kötelező'
+      return false;
+   }
+      error.innerHTML = ''//az erro.innerHTML lenullázsa
+   return true;
+}
--- main.js
+++ main.js
@@ -28,50 +28,30 @@ let array = [
     },
 ]
 
-
+//függvények meghívása és feltételek megadása
 createHTMLelement('table','person_table',document.body);
 create_HTML_element_with_parent_id('thead','person_thead','person_table');
 create_HTML_element_with_parent_id('tr','person_tr','person_thead');
-
 createTableHeaderCells();
-
-
 create_HTML_element_with_parent_id('tbody','person_tbody','person_table');
 
-
+//egy függvény ami új cellákat ad a táblázathoz amikor a feltöltés gomb megnyomásra kerül
 const form = document.getElementById('form');
-form.addEventListener('submit',function(e){
+form.addEventListener('submit',function(e){//itt adjuk az utasítást, hogy figyeljen a gomlenyomásra és tegye azt ami a zárójeleken belül van
     e.preventDefault()
-    const lastname = document.getElementById('lastname');
-    const firstname1 = document.getElementById('firstname1');
-    const firstname2 = document.getElementById('firstname2');
-    const married = document.getElementById('married');
-    const pet = document.getElementById('pet');
 
-    
-
-    const firstname1Value = firstname1.value;
-    let firstname2Value = firstname2.value;
-    const lastnameValue = lastname.value;
-    const marriedValue = married.checked;
-    const petValue = pet.value;
-
-    if(firstname2Value === ''){
-        firstname2Value = undefined;
-    }
-
-        if(validateFields(lastname, firstname1, pet)){
+        if(validateFields(lastname, firstname1, pet)){//Ha a függvény meg van hívva, vegye fel az adatokat
             const newperson = {
-                firstname1: firstname1Value,
-                firstname2: firstname2Value,
-                lastname: lastnameValue,
-                married: marriedValue,
-                pet: petValue
+                firstname1: document.getElementById('firstname1').value,
+                firstname2: document.getElementById('firstname2').value,
+                lastname: document.getElementById('lastname').value,
+                married:document.getElementById('married').checked,
+                pet: document.getElementById('pet').value
             }
 
             array.push(newperson);
             renderTable(array);
-            form.reset();
+            form.reset();//ha le van nyomva a gomb és minden helyes, kitörli a rublikákból a szöveget
     }
     
 })
